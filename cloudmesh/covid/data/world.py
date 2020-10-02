7e22bbd8306b7447830b21485facb0a04caa40b8
Gregor von Laszewski <laszewski@gmail.com>
2020-10-02 14:21:41 -0400

    refactor

--- cloudmesh/covid/data/world.py
+++ cloudmesh/covid/data/world.py
@@ -4,8 +4,21 @@
 
 import requests
 import pandas as pd
+import numpy as np
 
-def get_covid_country_data():
+#Setting up the world countries data URL
+url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
+country_shapes = f'{url}/world-countries.json'
+
+
+def add_log(data, col):
+  data[f'log_{col}'] = np.log2(data[col] + 1) # log 0 is not defined, so smply add one
+  data = data.astype({f"log_{col}": int}) # convert column to integer
+  return data
+
+
+
+def get_covid_data():
 
   #download the data from he virustracker
   res = requests.get('https://api.thevirustracker.com/free-api?countryTotals=ALL')
@@ -30,3 +43,23 @@ def get_covid_country_data():
   # change title column to country column, so its easier to access when processing
   df = df.rename(columns={'title':'country'})
   return df
+
+
+
+def plot_map(title, data, col):
+  # we ignore the title for now
+  m = folium.Map()
+  #Adding the Choropleth layer onto our base map
+  folium.Choropleth(
+      #The GeoJSON data to represent the world country
+      geo_data=country_shapes,
+      name=title,
+      data=data,
+      #The column aceppting list with 2 value; The country name and  the numerical value
+      columns=['country', col],
+      key_on='feature.properties.name',
+      fill_color='PuRd',
+      nan_fill_color='white'
+  ).add_to(m)
+  return m
+
